3ade478394ec9b2ed0bcfdddf9c24693494b1ade
Braden <bradenmadgett@gmail.com>
2019-04-28 17:46:29 -0500

    Initial poll creation functionality
    Initial design for poll creation functionality

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ function addChoice(choice) {
     choicesContainer.appendChild(choiceEntry);
 }
 
+function addChoiceInput() {
+    var choiceLi = document.createElement("li");
+    var choiceInput = document.createElement("input");
+    choiceLi.setAttribute("class", "choice");
+    choiceInput.setAttribute("type", "text");
+    choiceInput.setAttribute("placeholder", "Option");
+    choiceLi.appendChild(choiceInput);
+    document.getElementById("choices").appendChild(choiceLi);
+}
+
 function initializeChoiceEntry(choice) {
     return {choiceName: choice, voteCount: 0};
 }
@@ -49,4 +59,59 @@ function buttonClicked(event){
     console.log("Button " + this.id + " clicked");
     updateVoteCount(this.id);
     updateChoiceButton(this.id);
+}
+
+function createPoll() {
+    var pollWrapper
+}
+
+function buildPoll() {
+
+}
+
+var votes = [2,6,3,1,4];
+
+function selected(id) {
+  removeSelected();
+  document.getElementById(id).className += " selected";
+}
+
+function removeSelected() {
+  var choices = document.getElementsByClassName("choice");
+  for(var i = 0; i < choices.length; i++)
+    {
+      choices[i].className = "choice";
+    }
+}
+
+function submit() {
+  console.log("Submit clicked!");
+  document.getElementById("submitOverlay").className = "show";
+  document.getElementById("submitConfirmation").className = "show";
+  document.getElementById("checkmarkContainer").className = "show";
+  document.getElementById("checkmark").className = "show";
+  setTimeout(function(){   
+    document.getElementById("submitOverlay").className = "";
+    document.getElementById("submitConfirmation").className = "";
+    document.getElementById("checkmarkContainer").className = "";
+    document.getElementById("checkmark").className = ""; 
+  setPercentages()}, 2000);
+}
+
+function setPercentages() {
+  removeSelected();
+  console.log("Set Percentages!");
+  var opts = document.getElementsByClassName("percentageOverlay");
+  var total = 0;
+  for (var i = 0; i < votes.length; i++)
+  {
+      total += votes[i];
+  }
+  console.log(total);
+  console.log(opts.length);
+  for (var f = 0; f < opts.length; f++)
+  {
+      var percentage = (votes[f]/total)*100;
+      opts[f].style.width = String(percentage) + "%";
+  }
 }
